79ba5e972a78404a854e70a5de5847f94290a873
OlhaKur <kylovalola55@gmail.com>
2023-03-30 10:57:26 +0300

    done 4 lessons

--- src/route/index.js
+++ src/route/index.js
@@ -5,6 +5,35 @@ const router = express.Router()
 
 // ================================================================
 
+var header = {
+  name: {
+    firstname: 'Olexy',
+    lastname: 'Ivanov',
+  },
+  position: 'Junior Fullstack JS Developer',
+  salary: '600$ на місяць',
+  address: 'Ukrane, Kyiv',
+}
+
+var footer = {
+  social: {
+    email: {
+      text: 'ivanov@mail.com',
+      href: 'mailto:ivanov@mail.com'
+    },
+    phone: {
+      text: '+380670000123',
+      href: 'tel:+380670000123',
+    },
+    linkedIn: {
+      text: 'LinkedIn',
+      href: 'https://www.linkedin.com/in/dmytro-test',
+    },
+  },
+}
+
+//========================================================================
+
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
@@ -22,6 +51,240 @@ router.get('/', function (req, res) {
 router.get('/summary', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
+
+    page: {
+      title: 'Resume | Summary',
+
+    },
+
+    header,
+    main: {
+      summary: {
+        title: 'Summary',
+        text: `Open-minded for new technologies, with 1 years of
+        experience in development.Whenever I start to
+        work on a new project I learn the domain and try
+        to understand the idea of the project.Good team
+        player, every colleague is a friend to me.`,
+      },
+
+      experience: {
+        title: 'Other experience',
+
+        text: `Pet project for parsing sport betting data from
+        different platforms(odds) and sport statistics
+          (tournament position, goals etc), analyzing by
+        simple mathematics models and preparing
+        probability for such events like: money line -
+        first win / draw / second win, totals etc.`,
+      },
+    },
+    footer,
+
+
+    // ↙ сюди вводимо JSON дані
+  })
+})
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/skills', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('skills', {
+
+    page: {
+      title: 'Resume | Skills',
+
+    },
+
+    header,
+    main: {
+      skills: [
+        {
+          name: 'HTML',
+          point: 10,
+          isTop: true,
+        },
+        {
+          name: 'Handlebars',
+          point: 9,
+          isTop: false,
+        },
+        {
+          name: 'VS Code & NPM',
+          point: 10,
+          isTop: true,
+        },
+        {
+          name: 'Git & Terminal',
+          point: 9,
+        },
+        {
+          name: 'React.js',
+          point: 0,
+        },
+        {
+          name: 'PHP',
+          point: null,
+        },
+
+      ],
+
+      hobbies: [
+        {
+          name: 'Yoga',
+          isMain: true,
+        },
+        {
+          name: 'Car sporting',
+          isMain: true,
+        },
+        {
+          name: 'Photography',
+          isMain: false,
+        },
+        {
+          name: 'Mechanics',
+          isMain: true,
+        },
+        {
+          name: 'Knitting',
+          isMain: false,
+        },
+      ],
+
+    },
+    footer,
+
+
+    // ↙ сюди вводимо JSON дані
+  })
+})
+
+router.get('/education', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('education', {
+
+    page: {
+      title: 'Resume | Education',
+
+    },
+
+    header,
+    main: {
+      education: [
+        {
+          name: 'Міжнародний науково-технічний університет імені академіка Юрія Бугая (МНТУ)',
+          isEnd: true,
+        },
+        {
+          name: 'IT STEP University',
+          isEnd: true,
+        },
+        {
+          name: 'IT-BRAINS school',
+          isEnd: false,
+        },
+        {
+          name: 'Запорізький національний університет (ЗНУ)',
+          isEnd: true,
+        },
+        {
+          name: 'Інститут післядипломної освіти КПІ ім. Ігоря Сікорського',
+          isEnd: false,
+        },
+      ],
+
+      certificates: [
+        {
+          name: 'ЗНУПО',
+          id: 4356,
+        },
+        {
+          name: 'KDREW',
+          id: 8765,
+        },
+        {
+          name: 'LKJH',
+          id: 2347,
+        },
+
+      ],
+
+    },
+    footer,
+
+
+    // ↙ сюди вводимо JSON дані
+  })
+})
+
+router.get('/work', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('work', {
+
+    layout: 'big',
+
+    page: {
+      title: 'Resume | Work',
+    },
+
+    header,
+
+    main: {
+      works: [
+        {
+          position: 'Junior Fullstack Developer',
+          company: {
+            name: 'IT Brains',
+            url: 'http://it-brains.com.ua/',
+          },
+
+
+          duration: {
+            from: '10.10.2021',
+            to: '10.08.2022',
+          },
+          projectAmount: '3',
+          projects: [
+            {
+              name: 'Resume',
+              url: 'http://resume.com.ua/',
+              about: 'High-load application for searching exercises',
+              stacks: [
+                {
+                  name: 'React.js',
+
+                },
+                {
+                  name: 'HTML/CSS',
+
+                },
+                {
+                  name: 'Node.js',
+
+                },
+              ],
+              stackAmount: '4',
+              awards: [
+                {
+                  name: 'Award 1',
+                },
+                {
+                  name: 'Award 2',
+                },
+              ],
+              awardAmount: '8',
+            },
+          ],
+        },
+
+      ],
+
+    },
+
+    footer,
+
+
     // ↙ сюди вводимо JSON дані
   })
 })
